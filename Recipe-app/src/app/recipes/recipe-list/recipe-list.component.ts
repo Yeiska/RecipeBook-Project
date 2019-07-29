79d58bba9330717701110dce9c496a0674667860
Yeiska <ydelatorrefernand>
2019-07-29 16:06:25 -0400

    working on recipe-list and sopping list

---
+++ Recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply test', 'https://bmexdi064h-flywheel.netdna-ssl.com/wp-content/uploads/2017/09/Pesto-Pasta-Recipe-foodiecrush.com-002.jpg')
+  ];
+    
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
