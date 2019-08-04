b4b88323d757cd04527c50ac4d4b5790070c82b4
Yeiska <ydelatorrefernand>
2019-08-04 15:28:34 -0400

    adding dopdown on shoppingList and recipeList

--- Recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -8,8 +8,8 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
+  //@ViewChild('nameInput') nameInputRef: ElementRef;
+  //@ViewChild('amountInput') amountInputRef: ElementRef;
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
@@ -18,10 +18,10 @@ export class ShoppingEditComponent implements OnInit {
   }
 
   onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    //const ingName = this.nameInputRef.nativeElement.value;
+    //const ingAmount = this.amountInputRef.nativeElement.value;
+    //const newIngredient = new Ingredient(ingName, ingAmount);
+    //this.ingredientAdded.emit(newIngredient);
   }
 
 }
